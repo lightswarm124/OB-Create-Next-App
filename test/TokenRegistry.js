94a912117b2aee52fa3a8a611b27d208ac55cbf4
lightswarm124 <lightswarm@gmail.com>
2018-02-24 11:52:20 -0500

    test tokenregistry.js

--- test/TokenRegistry.js
+++ test/TokenRegistry.js
@@ -2,7 +2,10 @@ const OpenBounty = artifacts.require('./WIP/OpenBounty.sol');
 const TokenRegistry = artifacts.require('./WIP/TokenRegistry.sol');
 
 contract('Test TokenRegistry', (accounts) => {
-	it('Test TokenRegistry for OpenBounty', async () => {
-		let registry = await TokenRegistry.deployed();
+	it('Test TokenRegistry for OpenBounty', async function () {
+		const owner = accounts[0];
+		const newRegistry = await TokenRegistry.new({ from: owner });
+		const newOwner = await newRegistry.transferOwnership(accounts[1], { from: owner });
+		const newOwner2 = await newRegistry.transferOwnership(owner, { from: newOwner });
 	});
 });

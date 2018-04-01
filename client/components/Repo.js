1fb1f0dc56dac2bf795b0c5807a12a249859f07e
lightswarm124 <lightswarm@gmail.com>
2018-04-01 15:11:38 -0400

    Repo card Component

--- client/components/Repo.js
+++ client/components/Repo.js
@@ -1,46 +1,45 @@
 import React, { Component } from 'react'
-import fetch from 'isomorphic-fetch'
+import Link from 'next/link';
 
-
-class Repos extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			repos: [],
-			loading: true,
-		}
-	}
-
-	fetchRepos = async () => {
-		const response = await fetch(`https://api.github.com/users/lightswarm124/repos`)
-		const data = await response.json()
-
-		return this.setState({
-			loading: false,
-			repos: data,
-		})
-	}
-
-	componentDidMount() { return this.fetchRepos() }
-
-	render() {
-		return (
-			<article id="devRepositories">
-				{this.state.loading}
-
-				{this.state.repos.map(repo => !repo.fork && (
-					<div className="repo">
-						<a href={repo.svn_url} target="_blank">
-							<h4>{repo.name}</h4>
-							<p>{repo.description}</p>
-							<p>{repo.statuses_url}</p>
-						</a>
-
-
-					</div>
-				))}
-
+const Repo = ({...props}) => {
+	return (
+			<div className="repo">
+				<Link href={{ pathname: '/repo', query: { key: props.id, user: props.owner.login, name:props.name } }}>
+					<a>Repo Name: {props.name}</a>
+				</Link><br />
+				<Link href={`https://github.com/${props.owner.login}/${props.name}`}>
+					<a>Repo ID: {props.id}</a>
+				</Link><br />
+				<a>Repo Description: {props.description}</a><br />
+				<Link href={`https://github.com/${props.owner.login}`}>
+					<a>Repo Owner: {props.owner.login}</a>
+				</Link><br />
+				<a>Owner_ID: {props.owner.id}</a><br />
+				<a>Created At: {props.created_at}</a><br /><br />
+				<div className="buttons">
+					<button>Deploy Contract</button>
+					<button>Project Owner</button>
+					<button>Contract Address</button>
+					<button>Project Manager(s)</button>
+					<button>Lock Project</button>
+					<button>Unlock Project</button>
+				</div>
 				<style jsx>{`
+					.repo {
+						padding: 10px 10px 10px;
+						border: 1px solid #9B9B9B;
+					}
+					. buttons {
+						top: 0px;
+						right: 0px;
+					}
+				`}</style>
+			</div>
+	)
+}
+
+export default Repo;
+/*
 					#devRepositories {
 						display: -webkit-flex;display: -moz-flex;display: -ms-flex;display: -o-flex;display: flex;
 						justify-content: space-between;
@@ -143,11 +142,4 @@ class Repos extends React.Component {
 						}
 						.repo { margin: 20px; border: 1px solid #fff; }
 						.repo svg { display: none; }
-					}
-				`}</style>
-			</article>
-		)
-	}
-}
-
-export default Repos;
+*/

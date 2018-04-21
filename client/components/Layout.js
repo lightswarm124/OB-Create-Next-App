b4b6df9100022dc35194243fac90a9298c4351a2
lightswarm124 <lightswarm@gmail.com>
2018-04-21 15:54:02 -0400

    blah

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -1,19 +1,14 @@
 import Head from './Head';
 import Nav from './Nav';
-import React from 'react';
 
-class Layout extends Components {
-	render() {
-		return(
-			<div>
-		        <Head title={props.title} description={props.description} />
-		        <Nav/>
-		        <div className="container">
-		            {props.children}
-		        </div>
-		    </div>
-		);
-	}
-}
+const Layout = (props) => (
+    <div>
+        <Head title={props.title} description={props.description} />
+        <Nav/>
+        <div className="container">
+            {props.children}
+        </div>
+    </div>
+);
 
 export default Layout;
